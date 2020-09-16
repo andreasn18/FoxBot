d32ed8d3c3d8f2556e2d6c2aa8cf8906aa64f560
andreasn18 <andreasn18@users.noreply.github.com>
2020-09-16 16:21:46 +0700

    asd

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('ready', () => {
 
 
 bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
+    let args = message.content.substring(PREFIX.length).trim().split(" ");
     console.log(args);
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
     if (message.content.startsWith(PREFIX)) {
@@ -103,17 +103,18 @@ bot.on('message', message => {
 
 function image(message) {
     var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + "dogs",
+        url: "https://yandex.com/images/search?text=" + "anime",
         method: "GET",
-        header: {
+        headers: {
             "Accept": "text/html",
             "User-Agent": "Chrome"
         }
     };
 
-    request(options, function (error, response, responseBody) {
-        if (error)
+    request(options, function(error, response, responseBody){
+        if (error){
             return;
+        }
 
         $ = cheerio.load(responseBody);
         var links = $(".image a.link");
